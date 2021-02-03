ad94f5de604a851ca767d8ac88b92fa762162283
opob7414 <olivier.briand@orange.Com>
2021-02-03 16:43:00 +0100

    fullscreen is now working

--- dashboard-psg.py
+++ dashboard-psg.py
@@ -10,11 +10,12 @@ DARK_HEADER_COLOR = '#1B2838'
 
 layout = [[sg.Text('Block 3', font='Any 20')], [sg.Button('Go'), sg.Button('Exit')]]
 
-window = sg.Window('Dashboard PySimpleGUI-Style', layout, background_color=BLACK, resizable=True, no_titlebar=True, location=(0,0), size=(800,600)).Finalize()
+window = sg.Window('Dashboard PySimpleGUI-Style', layout, background_color=BLACK, resizable=True, finalize = True)
 window.Maximize()
 
 while True:             # Event Loop
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
+
 window.close()
