03725f373915db18b328e14c19351dbabde8a13d
opob7414 <olivier.briand@orange.Com>
2021-02-03 15:55:57 +0100

    add a dashboard source file with pysimplegui

---
+++ dashboard-psg.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+
+"""
+    Dashboard using blocks of information.
+    Copyright 2020 PySimpleGUI.org
+"""
+
+BLACK = '#000000'
+DARK_HEADER_COLOR = '#1B2838'
+
+layout = [[sg.Text('Block 3', font='Any 20')], [sg.Button('Go'), sg.Button('Exit')]]
+
+window = sg.Window('Dashboard PySimpleGUI-Style', layout, background_color=BLACK, resizable=True, no_titlebar=True, location=(0,0), size=(800,600)).Finalize()
+window.Maximize()
+
+while True:             # Event Loop
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+window.close()
