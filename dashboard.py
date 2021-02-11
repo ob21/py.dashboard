b8c4d0799e8a18e6e09de27cca7d8907aa44b280
ob21 <o.briand@gmail.com>
2021-02-11 23:29:43 +0100

    resize dashboard and remove title bar

--- dashboard.py
+++ dashboard.py
@@ -12,43 +12,44 @@ BPAD_LEFT = ((20,10), (0, 10))
 BPAD_LEFT_INSIDE = (0, 10)
 BPAD_RIGHT = ((10,20), (10, 20))
 
-top_banner = [[sg.Text('Dashboard'+ ' '*100, font='Any 20', background_color=DARK_COLOR),
-               sg.Text('Tuesday 9 June 2020', font='Any 20', background_color=DARK_COLOR),
-               sg.Button('X')]]
+top_banner = [[sg.Text('Dashboard', size=(25, 1), font='Any 20', background_color=DARK_COLOR),
+               sg.Text('Tuesday 9 June 2020', font='Any 20', 
+background_color=DARK_COLOR),
+               sg.Button(' X ')]]
 
-top  = [[sg.Text('The Weather Will Go Here', justification='c', pad=BPAD_TOP, font='Any 20')],
+top  = [[sg.Text('The Weather Will Go Here', justification='center', pad=BPAD_TOP, font='Any 18')],
             [sg.T(f'{i*25}-{i*34}') for i in range(7)],]
 
-block_3 = [[sg.Text('Block 3', font='Any 20')],
+block_3 = [[sg.Text('Block 3', font='Any 18')],
             [sg.Input(), sg.Text('Some Text')],
             [sg.Button('Go')]  ]
 
 
-block_2 = [[sg.Text('Block 2', font='Any 20')],
+block_2 = [[sg.Text('Block 2', font='Any 18')],
             [sg.T('This is some random text')],
             [sg.Image(data=sg.DEFAULT_BASE64_ICON)]  ]
 
-block_4 = [[sg.Text('Block 4', font='Any 20')],
+block_4 = [[sg.Text('Block 4', font='Any 18')],
             [sg.T('This is some random text')],
             [sg.T('This is some random text')],
             [sg.T('This is some random text')],
             [sg.T('This is some random text')]]
 
 
-layout = [[sg.Column(top_banner, pad=(0,0), background_color=DARK_COLOR)],
-          [sg.Column(top, size=(920, 90), pad=BPAD_TOP, background_color=DARK_COLOR)],
-          [sg.Column([[sg.Column(block_2, size=(450,150), pad=BPAD_LEFT_INSIDE)],
-                      [sg.Column(block_3, size=(450,150),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, 
+layout = [[sg.Column(top_banner, size=(760, 60), pad=(0,0), background_color=DARK_COLOR)],
+          [sg.Column(top, size=(760, 90), pad=BPAD_TOP, background_color=DARK_COLOR)],
+          [sg.Column([[sg.Column(block_2, size=(370,100), pad=BPAD_LEFT_INSIDE)],
+                      [sg.Column(block_3, size=(370,100),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, 
 background_color=BLACK_COLOR),
-           sg.Column(block_4, size=(450, 320), pad=BPAD_RIGHT)]]
+           sg.Column(block_4, size=(370, 220), pad=BPAD_RIGHT)]]
 
 window = sg.Window('Dashboard PySimpleGUI-Style', layout, background_color=BLACK_COLOR, 
-element_justification='center', resizable=True, finalize = True)
+element_justification='center', resizable=True, no_titlebar=True, finalize = True)
 window.Maximize()
 
 while True:             # Event Loop
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'X':
+    if event == sg.WIN_CLOSED or event == ' X ':
         break
 
 window.close()
